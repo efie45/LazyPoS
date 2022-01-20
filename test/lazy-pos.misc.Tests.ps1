d22dcfa15ab764c14717d5ebb4e19952aab6f411
Ethan Fie <efie45@gmail.com>
2022-01-19 20:35:24 -0600

    Split into multi libraries based on functional groups
    - Trying to split into multiple modules based on functionality
    - Want to make it easy to install just a small portion if that's all that's needed

---
+++ test/lazy-pos.misc.Tests.ps1
@@ -0,0 +1,29 @@
+Import-Module '../lazy-pos.misc/lazy-pos.misc.psm1' -Force
+
+Describe 'Lazy PoS Unit Tests' {
+    
+    #TODO Context 'Sort-VSCodeSettings'
+
+    Context 'Assert-PSVersion' {
+        $incorrectVersions = '8', 8, '8.0', '8.1.2', [version]'8.0'
+        It 'Throws error for incorrect versions' -Foreach $incorrectVersions {
+            { Assert-PSVersion -MinimumVersion $_ -CurrentVersion $([Version]'7.1.0') } | Should -Throw
+        }
+        $correctVersions = 7, '7.1.0', '7.1', '7.1', '7'
+        It "Doesn't throw error for correct versions" -Foreach $correctVersions {
+            { Assert-PSVersion -MinimumVersion $_ -CurrentVersion $([Version]'7.1.0') } | Should -Not -Throw
+        }
+    }
+
+    Context 'Convert-JsonToPSSyntax' {
+        <#
+            BUG
+            Needs to pipe to should operator
+            Isn't working as expected
+        #>
+        It 'Creates the correct object from example JSON' {
+            $json = Get-Content './example.json' -Raw
+            $json | ConvertTo-PSSyntax | Write-Host
+        }
+    }
+}
