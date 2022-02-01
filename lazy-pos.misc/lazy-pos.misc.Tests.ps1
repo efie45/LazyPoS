e3a6ef1d44ba29c9c07702e2c8e7dce375915352
Ethan Fie <efie45@gmail.com>
2022-02-01 07:00:31 -0600

    Add ConvertTo-Version

--- lazy-pos.misc/lazy-pos.misc.Tests.ps1
+++ lazy-pos.misc/lazy-pos.misc.Tests.ps1
@@ -1,23 +1,116 @@
-﻿BeforeAll {
-    Import-Module (Resolve-Path "$PSScriptRoot/../lazy-pos.misc/lazy-pos.misc.psm1")
+﻿using namespace System.Collections.Generic
+using namespace System.Text
+
+BeforeAll {
+    Import-Module (Resolve-Path "$PSScriptRoot/../lazy-pos.misc/lazy-pos.misc.psm1") -Force
 }
 
 Describe 'Lazy PoS Unit Tests' {
 
     #TODO Context 'Sort-VSCodeSettings'
 
-    Context 'Assert-PSVersion' {
+    Context 'Assert-PSVersion' -Skip {
         $incorrectVersions = '8', 8, '8.0', '8.1.2', [version]'8.0'
-        It 'Throws error for incorrect versions' -Foreach $incorrectVersions {
+        It 'Throws error for incorrect versions' -ForEach $incorrectVersions {
             { Assert-PSVersion -MinimumVersion $_ -CurrentVersion $([Version]'7.1.0') } | Should -Throw
         }
+
         $correctVersions = 7, '7.1.0', '7.1', '7.1', '7'
-        It "Doesn't throw error for correct versions" -Foreach $correctVersions {
+        It "Doesn't throw error for correct versions" -ForEach $correctVersions {
             { Assert-PSVersion -MinimumVersion $_ -CurrentVersion $([Version]'7.1.0') } | Should -Not -Throw
         }
     }
 
-    Context 'Convert-JsonToPSSyntax' {
+    Context 'ConvertTo-Version' {
+        It 'Should throw when non digits are provided' {
+            { '1.t1' | ConvertTo-Version } | Should -Throw -ExceptionType ([ArgumentException])
+        }
+
+        It 'Should throw when any subvalues are longer than 10 digits' {
+            # Arrange
+            $tooManyDigitsInMajor = { '12345678901.2.3' | ConvertTo-Version }
+            # Assert
+            $tooManyDigitsInMajor | Should -Throw -ExceptionType ([ArgumentException])
+        }
+
+        It 'Should write warning no minor version is provided' {
+            # Arrange
+            $warnings = [List[string]]::new()
+            Mock Write-Warning -Verifiable -ModuleName 'lazy-pos.misc' {
+                $warnings.Add($Message)
+            }
+            # Act
+            '1' | ConvertTo-Version
+            # Assert
+            Should -InvokeVerifiable
+            $warnings.Count | Should -BeExactly 1
+        }
+
+        It 'Should write warning when version will be truncated to 4 values' {
+            # Arrange
+            $warnings = [List[string]]::new()
+            Mock Write-Warning -Verifiable -ModuleName 'lazy-pos.misc' {
+                $warnings.Add($Message)
+            }
+            # Act
+            '1.2.3.4.5' | ConvertTo-Version
+            # Assert
+            Should -InvokeVerifiable
+            $warnings.Count | Should -BeExactly 1
+        }
+
+        It 'Should fail for decimal inputs over to 1.2147483647' {
+            { 1.2147483648 | ConvertTo-Version } | Should -Throw -ExceptionType ([ArgumentException])
+        }
+
+        $validInts = [List[int]]::new()
+        0..25 | ForEach-Object { $validInts.Add((Get-Random -Minimum 0 -Maximum ([int32]::MaxValue))) }
+        It 'Should work for valid int inputs' -ForEach $validInts {
+            # Arrange
+            Mock Write-Warning {} -ModuleName 'lazy-pos.misc'
+            # Act
+            $result = $_ | ConvertTo-Version
+            # Assert
+            $result | Should -BeOfType ([Version])
+        }
+
+        $validDecimals = [List[decimal]]::new()
+        0..25 | ForEach-Object {
+            $randomDec = Get-Random -Minimum 1.0 -Maximum 1.2147483647
+            $validDecimals.Add($randomDec)
+        }
+        It 'Should work for valid decimal inputs' -ForEach $validDecimals {
+            # Arrange
+            Mock Write-Warning {} -ModuleName 'lazy-pos.misc'
+            # Act
+            $result = $_ | ConvertTo-Version
+            # Assert
+            $result | Should -BeOfType ([Version])
+        }
+
+        $validStrings = [List[string]]::new()
+        0..25 | ForEach-Object {
+            $strBuilder = [StringBuilder]::new()
+            # Maximum is non-inclusive
+            1..(Get-Random -Minimum 1 -Maximum 5) | ForEach-Object {
+                $strBuilder.Append((Get-Random -Minimum 0 -Maximum ([int32]::MaxValue)))
+                $strBuilder.Append('.')
+            }
+            # Remove last '.'
+            $strBuilder.Remove($strBuilder.Length - 1, 1)
+            $validStrings.Add($strBuilder.ToString())
+        }
+        It 'Should work for valid string inputs' -ForEach $validStrings {
+            # Arrange
+            Mock Write-Warning {} -ModuleName 'lazy-pos.misc'
+            # Act
+            $result = $_ | ConvertTo-Version
+            # Assert
+            $result | Should -BeOfType ([Version])
+        }
+    }
+
+    Context 'Convert-JsonToPSSyntax' -Skip {
         <#
             BUG
             Needs to pipe to should operator
--- lazy-pos.misc/lazy-pos.misc.psm1
+++ lazy-pos.misc/lazy-pos.misc.psm1
@@ -1,23 +1,114 @@
-﻿function Assert-PSVersion {
-    #TODO: Documentation
+﻿using namespace System
+
+function Assert-PSVersion {
+    #TODO: Docs
     #TODO: Unit Tests
     <#
     .SYNOPSIS
     Validate the PowerShell version
+
+    .DESCRIPTION
+    To be used in situations where the [Microsoft Requires ]
     #>
+    [OutputType([bool])]
     [Cmdletbinding()]
     Param(
+
+        # Minimum version to check against. Can provide
         [Parameter(Mandatory)]
         [string]$MinimumVersion,
 
+        # Current version defaults to $PSVersionTable.PSVersion
         [Parameter()]
-        [Version]$CurrentVersion = $PSVersionTable.PSVersion
+        [Version]$CurrentVersion = $PSVersionTable.PSVersion,
+
+        # Throw an error if version doesn't meet requirements
+        [Parameter()]
+        [switch]$ThrowOnFalse
     )
     Process {
-        [Version]$min = $MinimumVersion.contains('.') ? $MinimumVersion : $MinimumVersion + '.0'
+        [Version]$min = $MinimumVersion
         if ($CurrentVersion -lt $min) {
-            throw "PSVersion must be at least $min to use this function"
+            if ($ThrowOnFalse) {
+                throw "PSVersion must be at least $min to use this function"
+            }
+            return $false
         }
+        return $true
+    }
+}
+
+function ConvertTo-Version {
+    <#
+    .SYNOPSIS
+    Converts a value to a .NET [Version] object
+
+    .DESCRIPTION
+    Converts a string, int, or decimal value to a .NET System.Version type.
+
+    Version.Parse() is often sufficient but it doesn't properly handle some situations such as single-digit values.
+    Single digit values will be given a 'minor' version number of 0 to be compatible with .NET version type requirements.
+
+    Version values in .NET are in the following format:
+      {major}.{minor}.{build}.{revision}
+
+    If only major version is provided a Version will be produced with value {major}.0
+    For example, providing '1' will give you a version with the following properties:
+
+    Major  Minor  Build  Revision
+    -----  -----  -----  --------
+    1      0      -1     -1
+
+    .EXAMPLE
+
+
+    .NOTES
+    Note that a Version type that doesn't specify a build or revision is considered 'less than'
+    a Version type that does specify these values. For example:
+
+    >> [Version]'1.0' -lt [Version]'1.0.0'
+    >> True
+
+    See the Microsoft Docs for more details:
+    https://docs.microsoft.com/en-us/dotnet/api/system.version
+
+    #>
+    [OutputType([Version])]
+    [Cmdletbinding()]
+    param(
+        # Value to be parsed into a Version type
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string]$Value
+    )
+    Process {
+        if ($Value -match '[^\d\.]') {
+            throw [ArgumentException] "Version can only contain chars 0-9 and '.'"
+        }
+
+        $split = $Value -split '\.'
+        $major, $minor, $build, $revision = $split
+
+        if ($split.Count -gt 4) {
+            'Version can only have a maximum of 4 version numbers. ' +
+            'Version will be truncated to format {major}.{minor}.{build}.{revision}' |
+                Write-Warning
+            $revision = $revision[0]
+        }
+
+        $major, $minor, $build, $revision | ForEach-Object {
+            if ([bigint]$_ -gt [int32]::MaxValue) {
+                $lengthError = "Part of version string '$_' exceeded maximum int32 value of $([int32]::MaxValue). " +
+                'This version cannot be converted to a .NET Version type'
+                throw [ArgumentException] $lengthError
+            }
+        }
+        if (-not $minor) {
+            'No minor version provided. Minor version required for .NET version type. Minor version ' +
+            'will be set to 0.' |
+                Write-Warning
+            $minor = 0
+        }
+        return [Version]"$major.$minor$($build ? ".$build" : '')$($revision ? ".$revision" : '')"
     }
 }
 
@@ -34,8 +125,7 @@ function ConvertTo-PSSyntax {
     You could export as JSON, keep in a file and use as a resource but if you want to use an object inline you can use
       this function like this:
 
-    $myObj | ConvertTo-JSON
-
+    $myObj | ConvertTo-PSSyntax
     .NOTES
     Not the most efficient way of doing this, but usually only used for small objects when creating tests.
     Could use refactoring for efficiency and additional edge cases if being used for different purposes.
@@ -69,6 +159,7 @@ function Invoke-SortVSCodeSetting {
     Alphabetically sort VSCode's settings json file in ascending or descending order
 
     #>
+    [Output([Void])]
     [Cmdletbinding()]
     Param(
         [Parameter()]
@@ -107,6 +198,7 @@ function Invoke-SortVSCodeSetting {
 function Get-RandomChar {
     #TODO: Documentation
     #TODO: Unit Tests
+    [Output([char])]
     [Cmdletbinding()]
     Param(
         [Alias('Upper')]
@@ -134,7 +226,7 @@ function Get-RandomChar {
 function Invoke-RandomizeClipboard {
     #TODO: Documentation
     #TODO: Unit Tests
-    [OutputType([System.Void])]
+    [OutputType([Void])]
     [Alias('cliprand')]
     [Cmdletbinding()]
     Param()
@@ -162,14 +254,6 @@ function Invoke-RandomizeClipboard {
         }
         $i++
     }
+    Write-Verbose "Copied randomized value to clipboard: $randomized"
     [string]::new($randomized) | Set-Clipboard
 }
-
-Export-ModuleMember -Function (
-    'Set-ClipboardWithNewGuid',
-    'Get-RandomChar',
-    'Invoke-RandomizeClipboard',
-    'Assert-PSVersion',
-    'Invoke-SortVSCodeSettings',
-    'ConvertTo-PSSyntax'
-)
